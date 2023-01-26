1ff8d3ccbd007e7be67c38c7c5635f3d98b3fd4c
Priyanshup891 <priyanshup891@gmail.com>
2023-01-26 13:56:54 +0530

    server_database_setup

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+
+dotenv.config();
+app.use(cors());
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
+const PORT = process.env.PORT || 5500;
+const mongo_username = process.env.MONGO_USERNAME;
+const mongo_password = process.env.MONGO_PASSWORD;
+const MONGO_URL = `mongodb://${mongo_username}:${mongo_password}@ac-xfs6l7q-shard-00-00.lsgh9rg.mongodb.net:27017,ac-xfs6l7q-shard-00-01.lsgh9rg.mongodb.net:27017,ac-xfs6l7q-shard-00-02.lsgh9rg.mongodb.net:27017/?ssl=true&replicaSet=atlas-emnr1m-shard-0&authSource=admin&retryWrites=true&w=majority`;
+
+mongoose
+  .connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server is running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`Something wrong with server: ${error}`));
